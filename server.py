fbb9e07b22b63f7b11c4e57281f454750dca9f6d
Cadenazar <88576308+Cadenazar@users.noreply.github.com>
2021-08-07 19:54:37 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,37 @@
+
+import flask
+from flask import Flask, render_template
+import os
+
+app = Flask("__main__")
+
+@app.route("/")
+
+def base_route():
+  return(render_template("Homepage.html"))
+
+@app.route("/Homepage")
+
+def homepage():
+  return(render_template("Homepage.html"))
+
+@app.route("/partselector")
+
+def partselector():
+    return(render_template("partselector.html"))
+
+@app.route("/Contact")
+
+def contact():
+    return(render_template("Contact.html"))
+
+
+@app.route("/results")
+
+def result():
+    return(render_template("results.html"))
+
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",port=os.getenv('PORT'))
